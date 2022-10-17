ea8f9f8c7e55742083693c394ddf50dfe8af5670
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-10-17 22:31:36 +0900

    feat: Generated 어노테이션으로 테스트 커버리지 예외

--- src/main/java/com/example/docker_ci/DockerCiApplication.java
+++ src/main/java/com/example/docker_ci/DockerCiApplication.java
@@ -1,8 +1,10 @@
 package com.example.docker_ci;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Generated // jacoco 테스트 커러리지 테스트시 무시함
 @SpringBootApplication
 public class DockerCiApplication {
 
